682e05351a9ec64ae9b7954adaa9704712b99df8
CgDjSong <manjukashyap425@gmail.com>
2025-07-19 10:50:13 +0530

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <meta name="google-site-verification" content="hkTy22ZYlsdntBotj2AKixlTey5VswH-DWZ7ua8wVSo" />
     <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
